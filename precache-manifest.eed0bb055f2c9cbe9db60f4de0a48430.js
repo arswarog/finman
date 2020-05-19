c03c0de2d11e1a108b4c64d6bc2e79c7df5bf2a8
Arswarog <arswarog@yandex.ru>
2020-05-19 10:30:55 +0300

    Updates

---
+++ precache-manifest.eed0bb055f2c9cbe9db60f4de0a48430.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54f5e50a02d0394b2e3a033461b156db",
+    "url": "/finman/index.html"
+  },
+  {
+    "revision": "f6e5fabce28acaa879b7",
+    "url": "/finman/static/css/main.8d1f00cc.chunk.css"
+  },
+  {
+    "revision": "a0c2a859e59213d4fe45",
+    "url": "/finman/static/js/2.8d9bb1ce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/finman/static/js/2.8d9bb1ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c3ecd0caf2b8bf27ecc",
+    "url": "/finman/static/js/3.d81b76ba.chunk.js"
+  },
+  {
+    "revision": "f6e5fabce28acaa879b7",
+    "url": "/finman/static/js/main.c720c848.chunk.js"
+  },
+  {
+    "revision": "4da4396ad0755b2aafde",
+    "url": "/finman/static/js/runtime-main.2a931aaa.js"
+  }
+]);
