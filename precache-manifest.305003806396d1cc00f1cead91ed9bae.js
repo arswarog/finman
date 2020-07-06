325c3fc5db74d8bb4d172076c2adea748cd60f23
Arswarog <arswarog@yandex.ru>
2020-07-06 11:28:21 +0300

    Updates

---
+++ precache-manifest.305003806396d1cc00f1cead91ed9bae.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84fc19e79dddb17becaccdf1a69a35c3",
+    "url": "/finman/index.html"
+  },
+  {
+    "revision": "3710ee8f63774320cb4f",
+    "url": "/finman/static/css/main.915df786.chunk.css"
+  },
+  {
+    "revision": "676ba601a59abf089150",
+    "url": "/finman/static/js/2.092213cc.chunk.js"
+  },
+  {
+    "revision": "e0cbdcd44a1274913b7732f016c7d0b6",
+    "url": "/finman/static/js/2.092213cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3710ee8f63774320cb4f",
+    "url": "/finman/static/js/main.73b1d650.chunk.js"
+  },
+  {
+    "revision": "4d2b8b651984deead807",
+    "url": "/finman/static/js/runtime-main.acadab5d.js"
+  },
+  {
+    "revision": "11fc83ae11617015f2fcde2065fb34d3",
+    "url": "/finman/static/media/dripicons-v2.11fc83ae.woff"
+  },
+  {
+    "revision": "7e12564e72400735ae5b671780f6a9f7",
+    "url": "/finman/static/media/dripicons-v2.7e12564e.eot"
+  },
+  {
+    "revision": "835984438066bd8944a7692e331152a7",
+    "url": "/finman/static/media/dripicons-v2.83598443.svg"
+  },
+  {
+    "revision": "cf09c981aeaa8736810133ab1148e4a3",
+    "url": "/finman/static/media/dripicons-v2.cf09c981.ttf"
+  }
+]);
