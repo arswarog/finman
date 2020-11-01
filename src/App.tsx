48aba6a3739b05e14752f79fc8599bc96b7c03cb
Лубяной Евгений <arswarog@users.noreply.github.com>
2020-11-01 22:13:48 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,43 +3,45 @@
  * @module app-root
  */
 
-import React from 'react';
-import styles from './App.module.scss';
-import { Redirect, Route, Switch } from 'react-router';
-import { HashRouter } from 'react-router-dom';
-import { NavBar } from './widgets/NavBar';
-import { useAction } from '@reatom/react';
-import { startListenOnlineStatus } from './atoms/client/client.service';
-import { store } from './store';
-import { initIndexedDB, useDBReady } from './db';
-import './sagas';
-import { sagaLauncher } from './sagas';
-import { paths, routes } from './routes';
-import { refresh } from './atoms/client/client.actions';
+import React from "react";
+import styles from "./App.module.scss";
+import { Redirect, Route, Switch } from "react-router";
+import { HashRouter } from "react-router-dom";
+import { NavBar } from "./widgets/NavBar";
+import { useAction } from "@reatom/react";
+import { startListenOnlineStatus } from "./atoms/client/client.service";
+import { store } from "./store";
+import { initIndexedDB, useDBReady } from "./db";
+import "./sagas";
+import { sagaLauncher } from "./sagas";
+import { paths, routes } from "./routes";
+import { refresh } from "./atoms/client/client.actions";
 
 export const App = () => {
-    const refreshAllHandler = useAction(() => {
-        console.log('*** refreshAllHandler');
+  const refreshAllHandler = useAction(() => {
+    console.log("*** refreshAllHandler");
 
-        initIndexedDB(store);
+    initIndexedDB(store);
 
-        console.log('*** refreshAllHandler complete');
+    console.log("*** refreshAllHandler complete");
 
-        return refresh();
-    }, []);
-    useDBReady(refreshAllHandler);
+    return refresh();
+  }, []);
+  useDBReady(refreshAllHandler);
 
-    return (
-        <div className={styles.App}>
-            <HashRouter>
-                <Switch>
-                    {routes.map((route, index) => <Route {...route} key={index}/>)}
-                    <Redirect to={paths.home()}/>
-                </Switch>
-                <NavBar/>
-            </HashRouter>
-        </div>
-    );
+  return (
+    <div className={styles.App}>
+      <HashRouter>
+        <Switch>
+          {routes.map((route, index) => (
+            <Route {...route} key={index} />
+          ))}
+          <Redirect to={paths.home()} />
+        </Switch>
+        <NavBar />
+      </HashRouter>
+    </div>
+  );
 };
 
 startListenOnlineStatus(store);
